3e5cab0e9213019f498e76de500f10aa343abab6
Ramandeep Bhagat <rdbhagat999@gmail.com>
2023-02-22 21:03:54 +0530

    adds single test for app component

--- src/app/app.component.cy.ts
+++ src/app/app.component.cy.ts
@@ -1,8 +1,23 @@
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { RouterModule } from "@angular/router";
+import { RouterTestingModule } from "@angular/router/testing";
 import { mount } from "cypress/angular";
 import { AppComponent } from "./app.component";
+import { AuthService } from "./_services";
+import { BACKEND_URL } from "./_shared/_models";
 
 describe("AppComponent", () => {
   it("mounts", () => {
-    mount(AppComponent);
+    mount(AppComponent, {
+      imports: [
+        HttpClientModule,
+        RouterModule,
+        NoopAnimationsModule,
+        AppComponent,
+      ],
+      providers: [BACKEND_URL, AuthService],
+    });
   });
 });
