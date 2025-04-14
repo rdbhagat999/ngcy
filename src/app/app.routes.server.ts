2d43400e14d9be00b68dacbd63cc852d16d40303
Ramandeep Bhagat <rdbhagat999@gmail.com>
2025-04-15 02:25:23 +0530

    implements getPrerenderParams

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,4 +1,9 @@
-import { RenderMode, ServerRoute } from "@angular/ssr";
+import { inject } from "@angular/core";
+import { PrerenderFallback, RenderMode, ServerRoute } from "@angular/ssr";
+import { AuthService, PostService } from "./_services";
+import { lastValueFrom } from "rxjs";
+import { RecipeService } from "./_services/recipe.service";
+import { ProductService } from "./_services/product.service";
 
 export const serverRoutes: ServerRoute[] = [
   {
@@ -8,6 +13,48 @@ export const serverRoutes: ServerRoute[] = [
   {
     path: "about",
     renderMode: RenderMode.Prerender,
+  },
+  {
+    path: "posts",
+    renderMode: RenderMode.Prerender,
+    fallback: PrerenderFallback.Server,
+    async getPrerenderParams() {
+      const service = inject(PostService);
+      const postResp = lastValueFrom(service.getPosts());
+      const posts = (await postResp).posts.map((post) => ({
+        id: `${post.id}`,
+      }));
+
+      return posts ? posts : [];
+    },
+  },
+  {
+    path: "recipes",
+    renderMode: RenderMode.Prerender,
+    fallback: PrerenderFallback.Server,
+    async getPrerenderParams() {
+      const service = inject(RecipeService);
+      const recipeResp = service.getRecipes();
+      const recipes = recipeResp()?.recipes.map((recipe) => {
+        return { id: `${recipe.id}` };
+      });
+
+      return recipes ? recipes : [];
+    },
+  },
+  {
+    path: "recipes",
+    renderMode: RenderMode.Prerender,
+    fallback: PrerenderFallback.Server,
+    async getPrerenderParams() {
+      const service = inject(ProductService);
+      const recipeResp = lastValueFrom(service.getProducts());
+      const products = (await recipeResp).products.map((product) => ({
+        id: `${product.id}`,
+      }));
+
+      return products ? products : [];
+    },
   },
   {
     path: "**",
